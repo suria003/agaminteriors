686a42783f4551438ddbc46fcaaa62a8cf710600
suriyaprakash <120815415+suria003@users.noreply.github.com>
2024-04-26 07:29:51 +0530

    app.py

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def view_estimate():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, port=8080)
